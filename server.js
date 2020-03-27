44cbcfb61132345a5c8cbbbcd0bb99de10615279
Carlos Junior <carlostrader09@gmail.com>
2020-03-27 15:56:35 +0000

    Aula 4 parte4

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const express = require("express")
 const server = express()
 
 
-// configurar arquivos estáticos (css, scripts, imagens
+// configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
 
 // criei uma rota /
 // e capturo o pedido do cliente para responder
 server.get("/", function(req, res) {
     return res.sendFile(__dirname + "/index.html")
+})
+
+server.get("/ideias", function(req, res) {
+    return res.sendFile(__dirname + "/ideias.html")
 })
 
 // liguei meu servidor na porta 3000
