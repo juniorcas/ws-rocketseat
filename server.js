0112bfa2f0646e7a5ea82b1a90ec010b7ef388d4
Carlos Junior <carlostrader09@gmail.com>
2020-03-27 18:10:44 +0000

    Aula 4 parte6

--- server.js
+++ server.js
@@ -2,6 +2,36 @@
 const express = require("express")
 const server = express()
 
+const ideas = [
+    {
+      img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+      title: "Cursos de Programacao",
+      category: "Estudo",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde labore perferendis ",
+      url: "https://rocketseat.com.br"
+    },
+    {
+      img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+      title: "Exercícios",
+      category: "Saúde",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde labore perferendis ",
+      url: "https://rocketseat.com.br"
+    },
+    {
+      img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+      title: "Meditacão",
+      category: "Mentalidade",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde labore perferendis ",
+      url: "https://rocketseat.com.br"
+    },
+    {
+      img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+      title: "Karaokê",
+      category: "Diversão em Família",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde labore perferendis ",
+      url: "https://rocketseat.com.br"
+    },
+]
 
 // configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
@@ -11,12 +41,18 @@ server.use(express.static("public"))
 const nunjucks = require("nunjucks")
 nunjucks.configure("views", {
   express: server, 
+  noCache: true, // boolean
 })
+
+
 
 // criei uma rota /
 // e capturo o pedido do cliente para responder
 server.get("/", function(req, res) {
-    return res.render("index.html")
+
+  
+  
+  return res.render("index.html", { ideas })
 })
 
 server.get("/ideias", function(req, res) {
